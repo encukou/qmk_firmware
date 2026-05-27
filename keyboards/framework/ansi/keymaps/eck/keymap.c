3d78db83b26d474621ed46ea713bfef9716e7285
Petr Viktorin <encukou@gmail.com>
2026-05-28 01:00:09 +0200

    Add keyboard layout too

---
+++ keyboards/framework/ansi/keymaps/eck/keymap.c
@@ -0,0 +1,89 @@
+// Copyright 2022 Framework Computer
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "framework.h"
+
+enum _layers {
+  _BASE,
+  _FN,
+  _FN_LOCK,
+  _FM
+};
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+     *         ┌─────┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬────┐
+     * 14 keys │ESC  │F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│Del │
+     *         ├───┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┤
+     * 14 keys │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │Backsp│
+     *         ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
+     * 14 keys │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │ \  │
+     *         ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴────┤
+     * 13 keys │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │ Enter │
+     *         ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───────┤
+     * 12 keys │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │  Shift  │
+     *         ├────┬───┼───┼───┼───┴───┴───┴───┴───┼───┼───┼───┴┬───┬────┤
+     *         │    │   │   │   │                   │   │   │────│↑  │────│
+     * 11 keys │Ctrl│FN │GUI│Alt│                   │Alt│Ctl├────┼───┼────┤
+     *         │    │   │   │   │                   │   │   │ ←  │  ↓│  → │
+     *         └────┴───┴───┴───┴───────────────────┴───┴───┴────┴───┴────┘
+     * 78 total
+     */
+    [_BASE] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+        MO(_FN), KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                    KC_PSCR, KC_RCTL, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT
+    ),
+     /*
+     * Function layer
+     *         ┌─────┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬──*┬───┬────┐ * Prt == GUI+S
+     * 14 keys │ESC  │Mut│vDn│vUp│Prv│Ply│Nxt│bDn│bUp│Scn│Air│Prt│App│Ins │
+     *         ├───┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┤
+     * 14 keys │   │B∞T│   │   │   │   │   │   │   │   │   │   │   │      │
+     *         ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
+     * 14 keys │     │   │RGB│Nxt│Hue│Sat│Spd│Brt│   │   │Pau│   │   │    │
+     *         ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴────┤
+     * 13 keys │      │   │   │Prv│Hue│Sat│Spd│Brt│ScL│   │   │   │       │
+     *         ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───────┤
+     * 12 keys │        │   │   │   │   │   │Brk│   │   │   │   │         │
+     *         ├────┬───┼───┼───┼───┴───┴───┴───┴───┼───┼───┼───┴┬───┬────┤
+     *         │    │   │   │   │                   │Com│   │────│PgU│────│
+     * 11 keys │    │   │   │   │ Toggle Backlight  │ po│   ├────┼───┼────┤
+     *         │    │   │   │   │                   │ se│   │Home│PgD│End │
+     *         └────┴───┴───┴───┴───────────────────┴───┴───┴────┴───┴────┘
+     * 78 total
+     */
+    [_FN] = LAYOUT(
+        KC_ESC,  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_BRIU, KC_SCRN, KC_AIRP, LGUI(KC_S), KC_MSEL,  KC_INS,
+        _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_SPI, RGB_VAI, _______, _______, KC_PAUS, _______, _______, _______,
+        _______, _______, _______, RGB_RMOD,RGB_HUD, RGB_SAD, RGB_SPD, RGB_VAD, KC_SCRL, _______, _______, _______,          _______,
+        KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, BL_BRTG, _______, KC_BRK,  _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______,          BL_STEP,                   _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
+    ),
+};
+
+// Make sure to keep FN Lock even after reset
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case FN_LOCK:
+            if (record->event.pressed) {
+                if (layer_state_is(_FN)) {
+                    set_single_persistent_default_layer(_FN_LOCK);
+                }
+                if (layer_state_is(_FM)) {
+                    set_single_persistent_default_layer(_BASE);
+                }
+            }
+            return false;
+            break;
+        default:
+            break;
+    }
+    return true;
+}
